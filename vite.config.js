b97f8259afe02160af39031d08f7db01076f6f7b
Robindev1812 <r85553272@gmail.com>
2024-08-25 23:59:10 -0400

    D

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/coursera-paradise-nursery-shopping-application/",
+  base: "/",
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
 })
