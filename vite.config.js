df13c56fe2789a32cc9f2fb4a6b0710cc19aaac6
Robindev1812 <r85553272@gmail.com>
2024-08-25 23:31:21 -0400

    a

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  base: "/shoppingreact",
-  plugins: [react()],
-})
+    base: "/shoppingreact",
+    build: {
+      outDir: 'build',
+    },
+    plugins: [react()],
+  })
